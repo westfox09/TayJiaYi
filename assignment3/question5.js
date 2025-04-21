cc4af6ceffe369b5a5e883852ab96ef1052e3276
westfox09 <westfox09@gmail.com>
2025-04-21 22:06:52 +0800

    Assignment 3

---
+++ assignment3/question5.js
@@ -0,0 +1,3 @@
+const bacData = [190.15, 194.21, 191.07, 192.44, 129000];
+
+// Write your code below
---
+++ assignment3/question6.js
@@ -0,0 +1,9 @@
+const disneyData = {
+	open: "120.54",
+	high: "122.56",
+	low: "120.54",
+	close: "121.09",
+	volume: "90000"
+};
+
+// Write your code below
